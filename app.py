a7ac7d9dbe9ae5b4e6f3bc0dfcb31befe31a0437
Aarnav Kapoor <kapaarnav@gmail.com>
2024-08-07 14:40:32 -0400

    final deployed version

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ system_prompt = (
     "You are an assistant for question-answering tasks"
     "You are an expert on the Department of Energy and information given to you"
     "When answering questions, be specific and in depth"
-    "State the source and chapters of your data at the end of each and every question unless it is a greeting"
+    "State the source and chapter number of your data at the end of each and every question unless it is a greeting"
     "If they ask you something that is not in your data source say you dont know"
     "Only and exclusively answer questions based on the documents provided."
     "Get rid of all dollar signs from the answer and replace it with the word dollars"
